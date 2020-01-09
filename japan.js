9f634f49a5fe4bbf82cce355174b921bfd25b9a5
imweb2019 <soso111so@daum.net>
2020-01-09 15:56:50 +0900

    update

--- japan.js
+++ japan.js
@@ -1,4 +1,5 @@
 $(window).load(function(){
+    jpFooterInit();
     jpOrderInit();
 
     $(document).on("click", ".jp_address_api", function(){
@@ -44,6 +45,25 @@ $(window).load(function(){
         $("#order_find_address").hide();
     });
 });
+
+function jpFooterInit(){
+    var loadCount=0;
+
+    var countCode = setInterval( function() {
+        loadCount++;
+        if($("footer#doz_footer_wrap ._policy_menu.policy_menu li._use_policy_menu").length>0){
+            $("footer#doz_footer_wrap ._policy_menu.policy_menu li._use_policy_menu").last().after('<li class="_use_policy_menu"><a href="#" onclick="openLaw(); return false;">特定商取り引きに関する法律に基づく表記</li>');
+
+            clearInterval(countCode);
+            countCode = "";
+            loadCount=0;
+        }else if(loadCount>20){
+            clearInterval(countCode);
+            countCode = "";
+            loadCount=0;
+        }
+    },200);
+}
 
 function jpOrderInit(){
     var loadCount=0;
@@ -113,7 +133,95 @@ function jpOrderInit(){
             loadCount=0;
         }
     },200);
-};
+}
+
+function openLaw(){
+    $.cocoaDialog.close();
+    var html = "";
+    html += '<div class="modal-header"><h4 class="modal-title">特定商取り引きに関する法律に基づく表記</h4><i class="btm bt-times bt-3x" data-dismiss="modal" aria-label="Close"></i></div>';
+    html += '<div class="modal-body">';
+    html += '<div class="li_board law_Area">';
+
+    // 판매업자
+    html += '<ul class="li_body">';
+    html += '<li>販売業者名</li>';
+    html += '<li>株式会社ムーンコーポレーション</li>';
+    html += '</ul>';
+
+    // 대표자
+    html += '<ul class="li_body">';
+    html += '<li>運営責任者名</li>';
+    html += '<li>キム イェソル</li>';
+    html += '</ul>';
+
+    // 소재지
+    html += '<ul class="li_body">';
+    html += '<li>所在地</li>';
+    html += '<li>韓国 ソウル市松坡区蚕室洞304-18 303</li>';
+    html += '</ul>';
+
+    // 전화번호
+    html += '<ul class="li_body">';
+    html += '<li>電話番号</li>';
+    html += '<li>050-5577-1161</li>';
+    html += '</ul>';
+
+    // 이메일
+    html += '<ul class="li_body">';
+    html += '<li>メールアドレス</li>';
+    html += '<li>cs@'+window.location.hostname+'</li>';
+    html += '</ul>';
+
+    // 사이트 URL
+    html += '<ul class="li_body">';
+    html += '<li>ホームページURL</li>';
+    html += '<li>https://'+window.location.hostname+'</li>';
+    html += '</ul>';
+
+    // 지불 방법
+    html += '<ul class="li_body">';
+    html += '<li>ホームページURL</li>';
+    html += '<li>クレジットカード(VISA・MASTER・JCB)<br/>銀行振込<br/>eContext</li>';
+    html += '</ul>';
+
+    // 상품 대금 이외 판매 금액
+    html += '<ul class="li_body">';
+    html += '<li>商品代金以外の必要料金</li>';
+    html += '<li>取扱商品はすべて税込価格となっております。<br/>別途消費税を頂くことはございません。</li>';
+    html += '</ul>';
+
+    // 배송안내
+    html += '<ul class="li_body">';
+    html += '<li>配送について</li>';
+    html += '<li>日本全国一律600円<br/>※Yamato Transport。<br/>※配送は日本国内に限ります。</li>';
+    html += '</ul>';
+
+    // 배송기간
+    html += '<ul class="li_body">';
+    html += '<li>商品のお届け期間</li>';
+    html += '<li>ご注文から 7~10日</li>';
+    html += '</ul>';
+
+    // 반품관련
+    html += '<ul class="li_body">';
+    html += '<li>返品について</li>';
+    html += '<li>';
+    html += '■ 返品先住所<br/>韓国 京畿道坡州市炭縣面丑峴里943 4洞ダブルユーツー<br/><br/>';
+    html += '■ 交換/返品案内<br/>交換、返品およびその他のお問い合わせは Q&Aにてお問合せください。<br/>';
+    html += '商品の不良など当社理由の場合、全額返金致します。<br/>';
+    html += 'お客様都合による交換・返品につきましては、返送代はお客様負担となります。<br/>';
+    html += 'お客様都合による返品につきましては、未開封の商品に限り交換または返金対応をさせて頂きます。<br/>';
+    html += '商品到着後7日以内の返品/返金が可能です。<br/>';
+    html += '出荷後に返金依頼をされた場合、出荷した商品を回収後に返金の処理を致します。<br/>';
+    html += 'クレジットカードで決済された場合、クレジットカード承認を取り消し、決済代金が請求されないように致します。<br/>';
+    html += '</li>';
+    html += '</ul>';
+
+    html += '</div>';
+    html += '</div>';
+
+	$.cocoaDialog.open({type: 'site_privacy', custom_popup: html});
+}
 
 function moncoTabInit(list, child, num){
     var num = num || 0;
